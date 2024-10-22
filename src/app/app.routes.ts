906c432f8b47449369d18c3ce0c30fb2855ac0e5
Fritz Blignaut <fritz.blignaut@gmail.com>
2024-10-22 20:56:07 +1000

    Created the Dashboard, Questions and Settings Components with teh Shared Header component. Navigation between these components have been implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)},
+    {path: 'questions', loadComponent: () => import('./questions/questions.component').then(c => c.QuestionsComponent)},
+    {path: 'settings', loadComponent: () => import('./settings/settings.component').then(c => c.SettingsComponent)},
+];
