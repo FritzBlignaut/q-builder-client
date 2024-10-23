6e3421965a7d50a13ff545e2ff1ed9ee644e3f71
Fritz Blignaut <fritz.blignaut@gmail.com>
2024-10-23 16:37:05 +1000

    Refactor questions component to use card component and implement question navigation

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { CardComponent } from '../shared/components/card/card.component';
 import { QuestionService } from './question.service';
 import { Observable } from 'rxjs';
 import { Question } from '../shared/models/question';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-questions',
   standalone: true,
-  imports: [CardComponent, AsyncPipe],
+  imports: [CardComponent, AsyncPipe, NgFor],
   templateUrl: './questions.component.html',
   styleUrl: './questions.component.scss'
 })
 export class QuestionsComponent implements OnInit {
+  @ViewChildren('questionSection') questionSections!: QueryList<ElementRef>;
+  
   questions$!: Observable<Question[]>;
 
   constructor(private questionService: QuestionService) {}
 
   ngOnInit() {
     this.questions$ = this.questionService.getQuestions();
+  }
+
+  scrollToNextQuestion(currentIndex: number) {
+    const sectionsArray = this.questionSections.toArray();
+    const nextIndex = currentIndex + 1;
+
+    if (nextIndex < sectionsArray.length) {
+      sectionsArray[nextIndex].nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToPreviousQuestion(currentIndex: number) {
+    const sectionsArray = this.questionSections.toArray();
+    const previousIndex = currentIndex - 1;
+
+    if (previousIndex >= 0) {
+      sectionsArray[previousIndex].nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
   }
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Question } from '../../models/question';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe, NgSwitch, NgSwitchCase],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  @Input() data!: Question[];
+  @Input() data!: Question;
 }
